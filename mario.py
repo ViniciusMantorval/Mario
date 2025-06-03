15b4cd761d0a3b6c4f0cbcc6ee1d57ba890d1d7e
ViniciusMantorval <VMGMaster0@gmail.com>
2025-06-03 08:40:23 -0300

    mario.py
    Version 1.1(06-03-2025)

--- mario.py
+++ mario.py
@@ -4,22 +4,32 @@ telaWidth=800
 telaHeight=600
 tela=tk.Tk()
 tela.geometry(f"{telaWidth}x{telaHeight}")
+tela.title("Mario")
 
+yvelocity=0
 canva = tk.Canvas(tela,width=800,height=600,bg="black")
 def gravity(obj):
-    grav=5
+    global yvelocity
+    grav=2
     cordinates = canva.coords(obj)
-    if cordinates[3]>=telaHeight-tamanho:
-        canva.move(obj,0,telaHeight-tamanho)
+    if cordinates[3]>telaHeight-tamanho:
+        yvelocity=0
+        canva.coords(mario,(x/2)-tamanho,y-tamanho,x/2,y)
     else:
-        canva.move(obj,0,grav)
+        if yvelocity<20 and yvelocity!=0:
+            yvelocity+=grav
+        canva.move(obj,0,yvelocity)
     canva.after(50,lambda:gravity(mario))
 x=telaWidth
 y=telaHeight
 tamanho=20
+def jump(event):
+    global yvelocity
+    yvelocity=-25   
+
 # mario = canva.create_rectangle((x/2)-tamanho,y-tamanho,x/2,y,fill="red")
 mario = canva.create_rectangle((x/2)-tamanho,0,x/2,tamanho,fill="red")
 canva.pack()
-
 gravity(mario)
+tela.bind("<space>", jump)
 tela.mainloop()

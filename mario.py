78179c514353570195873eb5e9f77da763512b1d
ViniciusMantorval <VMGMaster0@gmail.com>
2025-06-12 15:47:59 -0300

    Update mario.py

--- mario.py
+++ mario.py
@@ -6,30 +6,47 @@ tela=tk.Tk()
 tela.geometry(f"{telaWidth}x{telaHeight}")
 tela.title("Mario")
 
+xvelocity=10
 yvelocity=0
-canva = tk.Canvas(tela,width=800,height=600,bg="black")
+isOnFloor=False
+canva = tk.Canvas(tela,width=telaWidth,height=telaHeight,bg="black")
 def gravity(obj):
-    global yvelocity
-    grav=2
+    global x,xvelocity,yvelocity,isOnFloor
+    grav=4
     cordinates = canva.coords(obj)
-    if cordinates[3]>telaHeight-tamanho:
+    if cordinates[3]>telaHeight and not isOnFloor:
         yvelocity=0
         canva.coords(mario,(x/2)-tamanho,y-tamanho,x/2,y)
+        isOnFloor=True
     else:
-        if yvelocity<20 and yvelocity!=0:
+        if yvelocity<20 and not isOnFloor:
             yvelocity+=grav
-        canva.move(obj,0,yvelocity)
+        canva.move(obj,xvelocity,yvelocity)
     canva.after(50,lambda:gravity(mario))
 x=telaWidth
 y=telaHeight
 tamanho=20
 def jump(event):
-    global yvelocity
+    global yvelocity, isOnFloor
     yvelocity=-25   
+    isOnFloor=False
 
+def left(event):
+    global x,xvelocity
+    x-=xvelocity
+def right(event):
+    global x,xvelocity
+    x+=xvelocity
+# def on_key_release(event):
+#     global xvelocity
+#     if event.keysym == "a" or event.keysym == "d":
+#         xvelocity=0
 # mario = canva.create_rectangle((x/2)-tamanho,y-tamanho,x/2,y,fill="red")
 mario = canva.create_rectangle((x/2)-tamanho,0,x/2,tamanho,fill="red")
 canva.pack()
 gravity(mario)
 tela.bind("<space>", jump)
+tela.bind("<a>", left)
+tela.bind("<d>", right)
+# tela.bind("<KeyRelease>", on_key_release)
 tela.mainloop()
